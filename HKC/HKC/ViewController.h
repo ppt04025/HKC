6b9b30b87665980769fcb603ab1eb07e226ed189
he_shuang <hhhh@hhhdeMacBook-Air.local>
2015-08-04 07:03:42 +0800

    first commit

---
+++ HKC/HKC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HKC
+//
+//  Created by he_shuang on 15/8/4.
+//  Copyright (c) 2015年 zer0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HKC/HKC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HKC
+//
+//  Created by he_shuang on 15/8/4.
+//  Copyright (c) 2015年 zer0. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
